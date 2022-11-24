dbdd87a49b7399e8f05a26154d4f64801045cff1
Tat Tran <tattran22@outlook.com>
2022-11-24 21:32:17 +0700

    fix vite path alias resolve

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 
@@ -13,8 +14,6 @@ export default defineConfig({
     }),
   ],
   resolve: {
-    alias: {
-      '@': 'src/',
-    },
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
 })
